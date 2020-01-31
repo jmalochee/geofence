610610058a4e4cb416e385c342d1285e257f4b67
jmalochee <jmalochee@gmail.com>
2020-01-31 13:59:55 -0500

    MyMap component refactored with recompose

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -4,9 +4,21 @@ import {
 	withGoogleMap, 
 	GoogleMap, 
 	Marker 
-} from "react-google-maps"
+} from 'react-google-maps'
+import { compose, withProps } from 'recompose'
 
-const MyMap = withScriptjs(withGoogleMap((props) =>
+let url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD_e5jJiT7Y1R-QuKR19E23MTCbjQixC84&v=3.exp&libraries=geometry,drawing";
+
+const MyMap = compose(
+  withProps({
+    googleMapURL: url,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `600px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => 
   <GoogleMap
     defaultZoom={13}
     defaultCenter={{lat: 42.350668, lng: -71.077435}}
@@ -16,6 +28,6 @@ const MyMap = withScriptjs(withGoogleMap((props) =>
   >
     {props.isMarkerShown && <Marker position={{lat: 42.358169, lng: -71.063666}} />}
   </GoogleMap>
-));
+);
 
 export default MyMap;
